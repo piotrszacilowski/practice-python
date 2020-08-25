bd61df7971495ceea9efae54fbc7b5d6ed8ffdc4
Piotr Szaciłowski <szacilowski.piotr@gmail.com>
2020-08-25 18:13:27 +0200

    Add solution to task 07

---
+++ 07-list-comprehensions/task.py
@@ -0,0 +1,13 @@
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+even = [num for num in a if num % 2 == 0]
+
+print(even)
+
+# even = []
+#
+# for element in a:
+#     if element % 2 == 0:
+#         even.append(element)
+#
+# print(even)
